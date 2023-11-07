f498eb0666c7b71a475307b978f70e4ab4070888
jongomez <jon.olive.gomez@gmail.com>
2023-11-07 19:00:08 +0000

    update rogue storybookUrl and sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -42,16 +42,6 @@ const sidebars = {
           label: 'Color',
           id: 'visual-language/color',
         },
-        // {
-        //   type: 'doc',
-        //   label: 'Photography',
-        //   id: 'visual-language/photography',
-        // },
-        // {
-        //   type: 'doc',
-        //   label: 'Illustration',
-        //   id: 'visual-language/illustration',
-        // },
         {
           type: 'category',
           label: 'Typography',
@@ -101,17 +91,16 @@ const sidebars = {
             },
           ],
         },
-
-        {
-          type: 'doc',
-          label: 'Photography',
-          id: 'visual-language/photography',
-        },
-        {
-          type: 'doc',
-          label: 'Illustration',
-          id: 'visual-language/illustration',
-        },
+        // {
+        //   type: 'doc',
+        //   label: 'Photography',
+        //   id: 'visual-language/photography',
+        // },
+        // {
+        //   type: 'doc',
+        //   label: 'Illustration',
+        //   id: 'visual-language/illustration',
+        // },
       ],
     },
     {
